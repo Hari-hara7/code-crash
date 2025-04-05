fa0542bf4dd21618367cdb46e8bbab6ce9fd9ebf
Your Name <Hariharanath247@gmail.com>
2025-04-05 20:49:49 +0530

    hari

--- code_crash/next.config.ts
+++ code_crash/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  // Add more options if needed
 };
 
 export default nextConfig;
